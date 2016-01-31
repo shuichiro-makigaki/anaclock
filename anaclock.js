ea06d31b32c434c60cc46299b37c7ec58912310e
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2016-01-31 15:55:38 +0900

    Refactoring

--- anaclock.js
+++ anaclock.js
@@ -1,6 +1,24 @@
 'use strict';
 
-function anaclock() {
+var conf = {
+  "hand": {
+    "color": {
+      "r": 0,
+      "g": 0,
+      "b": 0
+    }
+  },
+  "background": {
+    "shape": "square",
+    "color": {
+      "r": 0,
+      "g": 0,
+      "b": 0
+    }
+  }
+};
+
+function drawClock(items) {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var d = new Date();
@@ -9,65 +27,91 @@ function anaclock() {
   var h = d.getHours();
   if (h > 12) h -= 12;
   var rad = 0;
-  var conf = {
-    "hand": {
-      "color": {"r": 0, "g": 0, "b": 0}
-    },
-    "background": {
-      "shape": "square",
-      "color": {"r": 0,"g": 0,"b": 0}
-    }
-  };
 
-  chrome.storage.local.get("conf", function(items) {
-    try {
-      conf = JSON.parse(items["conf"]);
-    } catch (e) {}
+  try {
+    conf = JSON.parse(items["conf"]);
+  } catch (e) {}
 
-    ctx.save();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.save();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Background
-    ctx.fillStyle = "rgb(" + conf.background.color.r + "," + conf.background.color.g + "," + conf.background.color.b + ")";
-    ctx.beginPath();
-    ctx.arc(canvas.width/2, canvas.height/2, canvas.width/2, 0, Math.PI*2, false);
-    ctx.fill();
+  // Background
+  ctx.fillStyle = "rgb(" + conf.background.color.r + "," + conf.background.color.g + "," + conf.background.color.b + ")";
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2, 0, Math.PI * 2, false);
+  ctx.fill();
 
-    // Face
-    ctx.strokeStyle = "rgb(" + conf.hand.color.r + "," + conf.hand.color.g + "," + conf.hand.color.b + ")";
-    ctx.beginPath();
-    ctx.moveTo(9, 0);
-    ctx.lineTo(9, 1);
-    ctx.moveTo(17, 9);
-    ctx.lineTo(18, 9);
-    ctx.moveTo(9, 17);
-    ctx.lineTo(9, 18);
-    ctx.moveTo(0, 9);
-    ctx.lineTo(1, 9);
-    ctx.stroke();
+  // Face
+  ctx.strokeStyle = "rgb(" + conf.hand.color.r + "," + conf.hand.color.g + "," + conf.hand.color.b + ")";
+  ctx.beginPath();
+  ctx.moveTo(9, 0);
+  ctx.lineTo(9, 1);
+  ctx.moveTo(17, 9);
+  ctx.lineTo(18, 9);
+  ctx.moveTo(9, 17);
+  ctx.lineTo(9, 18);
+  ctx.moveTo(0, 9);
+  ctx.lineTo(1, 9);
+  ctx.stroke();
 
-    ctx.translate(Math.ceil(canvas.width / 2), Math.ceil(canvas.height / 2));
+  ctx.translate(Math.ceil(canvas.width / 2), Math.ceil(canvas.height / 2));
 
-    // Short hand
-    rad = 2 * Math.PI / 12 * h + 2 * Math.PI / 12 / 60 * m - Math.PI / 2;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(Math.ceil(6 * Math.cos(rad)), Math.ceil(6 * Math.sin(rad)));
-    ctx.moveTo(0, 0);
-    ctx.lineTo(Math.ceil(2 * Math.cos(rad + Math.PI)), Math.ceil(2 * Math.sin(rad + Math.PI)));
-    ctx.stroke();
+  // Short hand
+  rad = 2 * Math.PI / 12 * h + 2 * Math.PI / 12 / 60 * m - Math.PI / 2;
+  ctx.moveTo(0, 0);
+  ctx.lineTo(Math.ceil(6 * Math.cos(rad)), Math.ceil(6 * Math.sin(rad)));
+  ctx.moveTo(0, 0);
+  ctx.lineTo(Math.ceil(2 * Math.cos(rad + Math.PI)), Math.ceil(2 * Math.sin(rad + Math.PI)));
+  ctx.stroke();
 
-    // Long hand
-    rad = 2 * Math.PI / 60 * m - Math.PI / 2;
-    ctx.moveTo(0, 0);
-    ctx.lineTo(Math.ceil(8 * Math.cos(rad)), Math.ceil(8 * Math.sin(rad)));
-    ctx.moveTo(0, 0);
-    ctx.lineTo(Math.ceil(2 * Math.cos(rad + Math.PI)), Math.ceil(2 * Math.sin(rad + Math.PI)));
-    ctx.stroke();
+  // Long hand
+  rad = 2 * Math.PI / 60 * m - Math.PI / 2;
+  ctx.moveTo(0, 0);
+  ctx.lineTo(Math.ceil(8 * Math.cos(rad)), Math.ceil(8 * Math.sin(rad)));
+  ctx.moveTo(0, 0);
+  ctx.lineTo(Math.ceil(2 * Math.cos(rad + Math.PI)), Math.ceil(2 * Math.sin(rad + Math.PI)));
+  ctx.stroke();
 
-    ctx.restore();
+  ctx.restore();
 
-    chrome.browserAction.setIcon({
-      "imageData": ctx.getImageData(0, 0, canvas.width, canvas.height)
-    });
+  chrome.browserAction.setIcon({
+    "imageData": ctx.getImageData(0, 0, canvas.width, canvas.height)
   });
+}
+
+
+function saveOptions(items) {
+  try {
+    conf = JSON.parse(items["conf"]);
+  } catch (e) {}
+
+  $('#hand-color').ColorPicker({
+    color: conf["hand"]["color"],
+    flat: true,
+    onChange: function(hsb, hex, rgb) {
+      conf["hand"]["color"] = rgb;
+      chrome.storage.local.set({
+        "conf": JSON.stringify(conf)
+      });
+    }
+  });
+
+  $('#bg-color').ColorPicker({
+    color: conf["background"]["color"],
+    flat: true,
+    onChange: function(hsb, hex, rgb) {
+      conf["background"]["color"] = rgb;
+      chrome.storage.local.set({
+        "conf": JSON.stringify(conf)
+      });
+    }
+  });
+}
+
+function anaclock() {
+  chrome.storage.local.get("conf", drawClock);
+}
+
+function options() {
+  chrome.storage.local.get("conf", saveOptions);
 }
